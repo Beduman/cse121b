dd1c7c270e573dd5a8177c81a72cd3a9ccba4e2a
Beduman <behunz77@gmail.com>
2023-09-23 23:02:38 -0700

    Week 2

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,26 +3,28 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
+let fullName = "Braden Hunsaker";
+let currentYear = 2023;
+let profilePicture = "images/brady.jpg";
 
 
 
 /* Step 3 - Element Variables */
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
 
 
 
 
 /* Step 4 - Adding Content */
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 
 
 
 /* Step 5 - Array */
-
-
+let favFood = ['Fettuccini Alfredo',' Biscuits and Gravy', ' Eggs in all forms'];
+foodElement.innerHTML = `<strong>${favFood}</strong>`;
 
 
 
