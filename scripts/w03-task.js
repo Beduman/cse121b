643051dc59b84f038bd2b4ca273a92de0c2bb3ca
Beduman <behunz77@gmail.com>
2023-10-02 16:09:18 -0700

    finally addition works

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -9,15 +9,13 @@ function add (number1, number2){
 function addNumbers() {
     let addNumber1 = Number(document.querySelector('#add1').value);
     let addNumber2 = Number(document.querySelector('#add2').value);
-    document.querySelector('#sum').value = (addNumber1, addNumber2);
-    document.querySelector("#addNumbers").addEventListener('click', addNumbers);
+    document.querySelector('#sum').value = (addNumber1 + addNumber2);
 
-    console.log("First Number: ", firstNumber);
-    console.log("Second Number: ", secondNumber);
+    console.log("First Number: ", addNumber1);
+    console.log("Second Number: ", addNumber2);
     console.log("Sum: ", sum);
-
 }
-
+document.querySelector("#addNumbers").addEventListener('click', addNumbers);
 /* Function Expression - Subtract Numbers */
 function subtract (number1, number2){
     return number1 + number2
@@ -26,21 +24,19 @@ function subtract (number1, number2){
 function subtractNumbers() {
     let subtractNumber1 = Number(document.querySelector('#subtract1').value);
     let subtractNumber2 = Number(document.querySelector('#subtract2').value);
-    document.querySelector('#difference').value = (subtractNumber1, subtractNumber2);
 
     console.log("First Number: ", firstNumber);
     console.log("Second Number: ", secondNumber);
     console.log("Difference: ", sum);
 }
-
+document.querySelector('#difference').value = (subtractNumber1 - subtractNumber2);
 /* Arrow Function - Multiply Numbers */
 const multiply = (factor1, factor2) => factor1 + factor2;
 
 
 const factorNumber1 = () => Number(document.querySelector('#factor1').value);
 const factorNumber2 = () => Number(document.querySelector('#factor2').value);
-const multiplyNumbers = () => document.querySelector('#product').value = (factorNumber1, factorNumber2);
-
+document.querySelector('#product').value = (factorNumber1, factorNumber2);
 /* Open Function Use - Divide Numbers */
 function divide (dividend, divisor){
     return dividend + divisor
@@ -49,12 +45,12 @@ function divide (dividend, divisor){
 function divideNumbers () {
     let dividend = Number(document.querySelector('dividend').value);
     let divisor = Number(document.querySelector('divisor').value);
-    document.querySelector('#quotient').value = (dividend, divisor);
 
     console.log("Dividend: ", dividend);
     console.log("Divisor: ", divisor);
     console.log("Quotient: ", quotient);
 }
+document.querySelector('#quotient').value = (dividend / divisor);
 /* Decision Structure */
 
 
