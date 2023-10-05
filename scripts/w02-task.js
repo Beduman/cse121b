8931af23c5fa517d056627e39619c16faffcfa21
Beduman <behunz77@gmail.com>
2023-10-04 17:12:35 -0700

    Fixed Week 2

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -13,18 +13,26 @@ let profilePicture = "images/brady.jpg";
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-
+const imageElement = document.querySelector('img');
 
 
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
-
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 
 /* Step 5 - Array */
 let favFood = ['Fettuccini Alfredo',' Biscuits and Gravy', ' Eggs in all forms'];
 foodElement.innerHTML = `<strong>${favFood}</strong>`;
+let favoritefoods = ['Ice Cream', 'Burgers'];
+favFood.push(favoritefoods);
+foodElement.innerHTML += `<br>${favFood}`;
+favFood.splice(0,1);
+foodElement.innerHTML += `<br>${favFood}`;
+favFood.pop();
+foodElement.innerHTML += `<br>${favFood}`;
 
 
 
