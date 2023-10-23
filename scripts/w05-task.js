933a6732750132a8bfe181f632a4437671583d32
Beduman <behunz77@gmail.com>
2023-10-23 11:13:30 -0700

    I'm trying

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,21 +1,22 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-const templesElement = document.getElementById('temples');
+const templesElement = document.getElementById('#temples');
 let templeList = [];
 
 /* async displayTemples Function */
 const displayTemples = (temples) => {
-    temples.array.forEach(element => {
-        let article = document.createElement('article');
-        let h3 = document.createElement('article');
-        let img = document.createElement('img');
+    temples.forEach((element) => {
+        const article = document.createElement('article');
+        const h3 = document.createElement('h3');
+        h3.textContent = temple.templeName;
+        const img = document.createElement('img');
+        img.src = temple.imageUrl;
         article.appendChild(h3, img);
     
         templesElement.appendChild(article);
     });
 }
-
 
 /* async getTemples Function using fetch()*/
 const getTemples = async () => {
@@ -24,12 +25,31 @@ const getTemples = async () => {
 }
 
 /* reset Function */
-
+function reset() {
+    templeList = []
+}
 
 /* sortBy Function */
-
-
-
-getTemples();
+function sortBy(temples) {
+    document.getElementById('temples');
+    reset();
+    const filter = document.getElementById('sortBy');
+    switch (filter) {
+        case 'utah':
+            displayTemples(temples.filter(temple => temple.location.includes('Utah')));
+            break;
+        case 'notutah':
+            displayTemples(temples.filter(temple => !temple.location.includes('Utah')));
+            break;
+        case 'older':
+            displayTemples(temples.filter(temple => new Date(temple.dedicatedDate) < new Date(1950, 0, 1)));
+            break;
+        case 'all':
+            displayTemples(temples);
+    }
+}
 
 /* Event Listener */
+document.querySelector('sortby').addEventListener('change', () => {sortBy(templeList)});
+
+getTemples();
