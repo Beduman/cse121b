4023f62425ffebd314587fdc2a243ee060e6b4ca
Beduman <behunz77@gmail.com>
2023-10-24 20:40:40 -0700

    Search bar still no work

--- scripts/project.js
+++ scripts/project.js
@@ -1,24 +1,46 @@
-const monsterselement = document.getElementById('monsters');
+const monstersElement = document.getElementById('monsters');
 let monsterList = [];
 
-const displayMonsters = (temples) => {
-    temples.forEach((element) => {
+function reset(element) {
+    element.innerHTML = '';
+}
+
+function displayMonsters(monsters) {
+    monsters.forEach((element) => {
         const article = document.createElement('article');
         const h3 = document.createElement('h3');
         h3.textContent = element.slug;
         const p = document.createElement('p');
         p.textContent = element.type;
-        const img = document.createElement('img');
-        img.src = element.imageUrl;
+        const s = document.createElement('p');
+        s.textContent = element.size;
+        const description = document.createElement('p');
+        description.textContent = element.desc
         article.appendChild(h3);
         article.appendChild(p);
-        article.appendChild(img);
-        monsterselement.appendChild(article);
+        article.appendChild(s);
+        article.appendChild(description);
+        monstersElement.appendChild(article);
     });
 }
 
+
 async function getMonsters () {
-    const response = await fetch("api.open5e.com/v1/monsters")
+    const response = await fetch("https://run.mocky.io/v3/eb41f4f1-e5ea-4443-bb79-2e282188da65");
     monsterList = await response.json();
     displayMonsters(monsterList);
 }
+
+function search (monsters) {
+    const monsterSearch = document.getElementById('search').value;
+    reset(monstersElement);
+
+    console.log('do we even get to this point?');
+    displayMonsters(monsters.filter(monsters => monsters.slug.includes(monsterSearch)));
+    console.log(monsterSearch);
+}
+
+
+document.getElementById('search').addEventListener('change', () => { search(monsterList)});
+
+getMonsters();
